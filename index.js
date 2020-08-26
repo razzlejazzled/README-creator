253180747ae751766395562c58028a1205611b23
RazzleJazzled <robertjgassner@gmail.com>
2020-08-26 14:29:16 -0700

    draft 1 written

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util =  require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 // array of questions for user
-const questions = [
+
+function promptUser() {
+    return inquirer.prompt([
     {
         type: "input",
         message: "What is the title of this project?",
@@ -22,8 +28,8 @@ const questions = [
     },
     {
         type: "checkbox",
-        message: "What is the license for this project?",
-        choices: "MIT", "Other",
+        message:"What is the license for this project?",
+        choices: ["MIT", "Apache", "MPL", "Other"],
         name: "license",
     },
     {
@@ -42,22 +48,64 @@ const questions = [
         name: "username",
     },
     {
-        type: "what email address would you like to associate with this project?",
+        type: "input",
+        message: "what email address would you like to associate with this project?",
         name: "email"
     }
 
 
-];
-
+]);
+}
 // function to write README file
-function writeToFile("ReadME.me", data + "\n") {
+function generateREADME(answers) {
+    return `
+    {answers.title}
+
+    Description:
+    {answers.description}
+
+    Table of Contents:
+        Description
+        Installation
+        Usage
+        License
+        Contributing
+        Tests
+        Questions
     
-} 
+    Installation:
+    {answers.install}
+
+    Usage:
+    {answers.usage}
+
+    License:
+    {answers.license}
+
+    Contributing:
+    {answers.contributing}
+
+    Tests:
+    {answers.tests}
+
+    Questions:
+
+    If you have any questions, please email me at {answers.email} or message my github profile: {answers.username}
+    `
+}
 
 
 // function to initialize program
-function init() {
-
+async function init() {
+    console.log("async")
+    try {
+        const answers = await promptUser();
+        const README = generateREADME(answers);
+        await writeFileAsync("./results/Readme.md", README);
+        console.log("Successfully wrote to README.md");
+    } catch(err){
+        console.log(err);
+    }
 }
 
 // function call to initialize program
